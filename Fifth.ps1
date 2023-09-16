20382b20c0444a4c01faa34137970ebd8af74d51
Iamneo <techadmin@iamneo.ai>
2023-09-16 15:33:50 +0000

    Backup Commit

--- Fifth.ps1
+++ Fifth.ps1
@@ -1,3 +1,15 @@
 new-item Testing -itemtype directory
 cd Testing
-new-itemtype su
+new-itemtype subfolder1 -itemtype directory
+new-itemtype subfolder2 -itemtype directory
+cd subfolder1
+new-item Test1.txt
+new-item Test2.txt
+new-item Test3.txt
+cd..
+cd subfolder2
+new-item Test4.txt
+new-item Test5.txt
+new-item Test6.txt
+cd/home/coder/project/workspace
+write-host "All folders and subfolders successfully created, Task 5 completed"
